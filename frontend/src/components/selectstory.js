5887497b8db23590bddf7ac9fad51f78acaabc74
Georgi Kalancha <61438711+GeorgiKal@users.noreply.github.com>
2023-05-14 15:08:02 -0400

    completed and functioning

--- frontend/src/components/selectstory.js
+++ frontend/src/components/selectstory.js
@@ -1,51 +1,77 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-//import { supabase } from '../utils/supabaseClient'; // import Supabase client
+import { FaPlay } from "react-icons/fa";
+import textToSpeech from "./textToSpeech";
 
-const SelectStory = ({supabase}) => {
-    const navigate = useNavigate();
-    const [stories, setStories] = useState([]); 
+const SelectStory = ({ supabase }) => {
+  const navigate = useNavigate();
+  const [stories, setStories] = useState([]);
 
-    useEffect(() => {
-        fetchStories();
-    }, []);
-
+  useEffect(() => {
     const fetchStories = async () => {
-        let { data: stories, error } = await supabase
-            .from('stories') // replace 'stories' with your actual table name
-            .select('*')
-            .order('created_at', { ascending: false })
-            .limit(3);
+      let { data: stories, error } = await supabase
+        .from("stories")
+        .select("*")
+        .order("created_at", { ascending: false })
+        .limit(3);
 
-        if (error) console.log("error", error);
-        else setStories(stories);
+      if (error) console.log("error", error);
+      else setStories(stories);
     };
 
-    const handleHome = () => {
-        navigate("/");
-    };
+    fetchStories();
+  }, []);
 
-    const handleStorySelection = (story) => {
-        navigate('/hearstory', { state: { story } });
-    };
+  const handleHome = () => {
+    navigate("/");
+  };
 
-    return (
-        <>
-            <h1 className="top-0 flex justify-center p-3 font-serif text-6xl font-bold tracking-wider text-white cursor-pointer"
-            onClick={handleHome}>fAIble</h1>
-            <div className="flex flex-row flex-wrap justify-center pt-10">
-                {stories.map((story, index) => (
-                    <button 
-                        key={index} 
-                        onClick={() => handleStorySelection(story)} 
-                        className="flex items-center justify-center w-64 mx-2 my-2 text-4xl text-white bg-blue-600 rounded-md h-96 hover:bg-purple-600"
-                    >
-                        {story.title} {/* replace 'title' with the actual column name for the story title in your table */}
-                    </button>
-                ))}
-            </div>
-        </>
-    );
-}
+  const handleStorySelection = async (story) => {
+    try {
+      const audioData = await textToSpeech(story.content);
+      const audioUrl = URL.createObjectURL(new Blob([audioData], {
+        type: "audio/mpeg",
+      }));
+
+      navigate("/hearstory", {
+        state: {
+          story,
+          audioUrl,
+        },
+      });
+    } catch (error) {
+      console.error("Error generating audio:", error);
+    }
+  };
+
+  const getRandomColor = () => {
+    const colors = ["#FF6633", "#FFB399", "#FF33FF", "#FFFF99", "#00B3E6", "#E6B333", "#3366E6", "#999966"];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
+  return (
+    <>
+      <h1
+        className="top-0 flex justify-center p-3 font-serif text-6xl font-bold tracking-wider text-white cursor-pointer"
+        onClick={handleHome}
+      >
+        fAIble
+      </h1>
+      <div className="flex flex-row flex-wrap justify-center pt-10">
+        {stories.map((story, index) => (
+          <button
+            key={index}
+            onClick={() => handleStorySelection(story)}
+            className="flex items-center justify-center w-64 mx-2 my-2 bg-blue-600 rounded-md h-96 hover:bg-purple-600"
+            style={{ backgroundColor: getRandomColor() }}
+          >
+            <FaPlay size={48} color="#FFFFFF" />
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default SelectStory;
--- frontend/src/components/text-to-voice.js
+++ frontend/src/components/text-to-voice.js
@@ -2,34 +2,38 @@ const https = require('https');
 const fs = require('fs');
 require('dotenv').config();
 
-const options = {
-  hostname: 'api.elevenlabs.io',
-  path: '/v1/text-to-speech/'+process.env.voice_id,
-  method: 'POST',
-  headers: {
-    'accept': 'audio/mpeg',
-    'xi-api-key': process.env.api_key,
-    'Content-Type': 'application/json'
-  }
+const generateVoiceFile = (text) => {
+  const options = {
+    hostname: 'api.elevenlabs.io',
+    path: '/v1/text-to-speech/' + process.env.REACT_APP_VOICE_ID,
+    method: 'POST',
+    headers: {
+      'accept': 'audio/mpeg',
+      'xi-api-key': process.env.REACT_APP_API_KEY,
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const req = https.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    const audioFile = fs.createWriteStream('audio.mp3');
+    res.pipe(audioFile);
+  });
+
+  req.on('error', (error) => {
+    console.error(error);
+  });
+
+  req.write(JSON.stringify({
+    text,
+    voice_settings: {
+      stability: 0.5,
+      similarity_boost: 0.5
+    }
+  }));
+
+  req.end();
 };
 
-const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`);
-
-  const audioFile = fs.createWriteStream('audio.mp3');
-  res.pipe(audioFile);
-});
-
-req.on('error', (error) => {
-  console.error(error);
-});
-
-req.write(JSON.stringify({
-  text: 'Hello this is a great day. London is amazing, I want to go to new york, I will be rich.',
-  voice_settings: {
-    stability: 0.5,
-    similarity_boost: 0.5
-  }
-}));
-
-req.end();
+module.exports = generateVoiceFile;
